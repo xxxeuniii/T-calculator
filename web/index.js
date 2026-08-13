5d2475cb678d1ae8caf0e71cdc93c825e0c64764
srd13265058427 <1491372173@qq.com>
2026-08-14 00:44:54 +0800

    fix: invalidate stale mobile cache

--- web/index.js
+++ web/index.js
@@ -2,4 +2,15 @@ import { registerRootComponent } from "expo";
 
 import App from "./src/App";
 
+if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    await Promise.all(registrations.map((registration) => registration.unregister()));
+    if ("caches" in window) {
+      const names = await caches.keys();
+      await Promise.all(names.filter((name) => name.startsWith("trade-tool-")).map((name) => caches.delete(name)));
+    }
+  });
+}
+
 registerRootComponent(App);
